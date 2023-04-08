49e10def59478c67920446d1bffec92679408a32
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-04-08 18:55:08 +0900

    Mapping Changed && Swagger2 연동 에러 해결중

--- src/main/java/com/boot/jsp/board/controller/BoardController.java
+++ src/main/java/com/boot/jsp/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,11 +14,12 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/board/v1/")
 public class BoardController {
     private final BoardService service;
 
     // findList
-    @RequestMapping("/board/v1/findList")
+    @GetMapping("findList")
     public String findList(Model model) {
         List<BoardDTO> list = service.findList();
 
